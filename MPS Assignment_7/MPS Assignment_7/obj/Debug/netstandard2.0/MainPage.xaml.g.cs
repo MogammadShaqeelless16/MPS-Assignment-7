f57c3f0560c31f9d4c0dc4fc48a45de44b606fb8
Shaqeel Less <54390756+MogammadShaqeelless16@users.noreply.github.com>
2020-11-12 14:53:25 +0200

    Updates

--- MPS Assignment_7/MPS Assignment_7/obj/Debug/netstandard2.0/MainPage.xaml.g.cs
+++ MPS Assignment_7/MPS Assignment_7/obj/Debug/netstandard2.0/MainPage.xaml.g.cs
@@ -17,8 +17,20 @@ namespace MPS_Assignment_7 {
     public partial class MainPage : global::Xamarin.Forms.ContentPage {
         
         [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
+        private global::Xamarin.Forms.ImageButton ListBtn;
+        
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
+        private global::Xamarin.Forms.ImageButton GridBtn;
+        
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
+        private global::Xamarin.Forms.ListView WeatherForecastList;
+        
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
         private void InitializeComponent() {
             global::Xamarin.Forms.Xaml.Extensions.LoadFromXaml(this, typeof(MainPage));
+            ListBtn = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.ImageButton>(this, "ListBtn");
+            GridBtn = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.ImageButton>(this, "GridBtn");
+            WeatherForecastList = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.ListView>(this, "WeatherForecastList");
         }
     }
 }
